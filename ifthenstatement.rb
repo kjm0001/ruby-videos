9d810c04af51ab121cdc87975d487124c03bbc2c
kjm0001 <kjm0001@gmail.com>
2018-04-10 14:44:06 +0000

    if elsif else statement

---
+++ ifthenstatement.rb
@@ -0,0 +1,14 @@
+# if logic
+system 'clear'
+
+my_number = rand(1..10) # generate random number between 1 and 10
+puts "I'm thinking of a number between 1 and 10. Can you guess it? "
+number = gets.to_i
+
+if number == my_number
+    puts "Holy crap, you guessed my number!"
+elsif number < 1 or number > 10
+    puts "I said between 1 and 10!"
+else
+    puts "Nope, my number was #{my_number} !!"
+end    
