fbc0522181ac7d3a0c7e691870eebd41e840c769
kjm0001 <kjm0001@gmail.com>
2018-04-10 14:18:31 +0000

    learn variable

---
+++ variable.rb
@@ -0,0 +1,29 @@
+# variables
+system 'clear'
+
+my_name = "John Elder" # lowercase var 
+puts my_name
+
+my_num = 41 # no quotes and treated as number
+puts my_num
+
+num1 = 1
+num2 = 9
+
+puts num1 + num2 # math operation happened
+
+num1 = "1" # quoted so now treated as string
+num2 = "9"
+
+puts num1 + num2 # string concatenation happened
+
+num1 = "1"
+num2 = 9
+
+puts num1.to_i + num2  # convert string into an integer
+
+num1 = 1
+num2 = 9
+num3 = num1 
+
+puts num3
