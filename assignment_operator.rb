c42768364ffd411d19c6400185ba58d34b7ef6e5
kjm0001 <kjm0001@gmail.com>
2018-04-10 14:24:24 +0000

    learn assignment operator

---
+++ assignment_operator.rb
@@ -0,0 +1,18 @@
+# assignment operator
+system 'clear'
+
+num1 = 1
+puts num1 += 5 # adds and assigns value back to var
+
+num1 = 10
+puts num1 -= 5 # subtracts and assigns value back to var
+
+# *= multiply
+# /= divide
+# %= modulus
+# **= exponent
+
+
+
+
+
